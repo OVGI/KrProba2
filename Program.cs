8883ed58ba60655c006ac8c6f360dcc1afab0aec
OVGI <girichevao@gmail.com>
2022-12-28 03:02:26 +0300

    Added task colution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив строк через пробел: ");
+string[] firstArray = Console.ReadLine()!.Split(' ');
+string[] secondArray = new string[firstArray.Length];
+
+
+void SecondArrayReceive(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+SecondArrayReceive(firstArray, secondArray);
+PrintArray(secondArray);
